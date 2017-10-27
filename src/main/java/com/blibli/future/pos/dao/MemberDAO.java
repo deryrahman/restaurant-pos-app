b469acb35bf3ca1e7469a74de0e79fb3e70d4d74
Iqbal <iqbalkhowarizmi@ymail.com>
2017-10-27 20:57:48 +0700

    Edited to comply to convention.

--- src/main/java/com/blibli/future/pos/dao/MemberDAO.java
+++ src/main/java/com/blibli/future/pos/dao/MemberDAO.java
@@ -1,4 +1,13 @@
 package com.blibli.future.pos.dao;
 
+import com.blibli.future.pos.entity.Member;
+
+import java.util.List;
+
 public interface MemberDAO {
+    public List<Member> getAllMembers();
+    public Member getMember(Long id);
+    public boolean createMember(Member member);
+    public void updateMember(Member member);
+    public void deleteItemById(Long id);
 }
--- src/main/java/com/blibli/future/pos/entity/Member.java
+++ src/main/java/com/blibli/future/pos/entity/Member.java
@@ -1,4 +1,58 @@
 package com.blibli.future.pos.entity;
 
 public class Member {
+    private Long ID;
+    private String name;
+    private String address;
+    private String email;
+
+    public Member() {
+    }
+
+    public Member(Long ID, String name, String address, String email) {
+        this.ID = ID;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ID : " + ID + '\n' +
+                "Name : " + name + '\n' +
+                "Address : " + address + '\n' +
+                "Email : " + email + '\n';
+    }
 }
