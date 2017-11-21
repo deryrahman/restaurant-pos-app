ba543bc06a9e9335b099f7c2662416c9c17ed75c
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-11-21 20:56:47 +0700

    Add initial category DAO

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAO.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAO.java
@@ -1,7 +1,13 @@
 package com.blibli.future.pos.restaurant.dao;
 
-/**
- * Created by dery on 11/21/17.
- */
-public class CategoryDAO {
+import com.blibli.future.pos.restaurant.Metadata;
+import com.blibli.future.pos.restaurant.model.Category;
+
+public interface CategoryDAO {
+    public boolean create(Category category);
+    public Category getById(int id);
+    public Category[] getAll();
+    public Category[] getWithMetadata(Metadata metadata);
+    public boolean delete(Category category);
+    public boolean update(Category category);
 }
--- restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAOMysql.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAOMysql.java
@@ -1,7 +1,58 @@
 package com.blibli.future.pos.restaurant.dao;
 
-/**
- * Created by dery on 11/21/17.
- */
-public class CategoryDAOMysql {
+import com.blibli.future.pos.restaurant.Metadata;
+import com.blibli.future.pos.restaurant.MySQLUtility;
+import com.blibli.future.pos.restaurant.model.Category;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CategoryDAOMysql implements CategoryDAO {
+    @Override
+    public boolean create(Category category) {
+        Connection conn = null;
+        try {
+            BasicDataSource ds = MySQLUtility.getDataSource();
+            conn = ds.getConnection();
+            System.out.println(category);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Category getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Category[] getAll() {
+        return new Category[0];
+    }
+
+    @Override
+    public Category[] getWithMetadata(Metadata metadata) {
+        return new Category[0];
+    }
+
+    @Override
+    public boolean delete(Category category) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Category category) {
+        return false;
+    }
 }
