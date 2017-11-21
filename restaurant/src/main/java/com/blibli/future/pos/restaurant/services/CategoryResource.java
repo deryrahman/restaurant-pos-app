173412e43f0ab59813f0e538375a99033ace153c
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-11-21 20:59:09 +0700

    Add services

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/services/CategoryResource.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/services/CategoryResource.java
@@ -1,6 +1,41 @@
 package com.blibli.future.pos.restaurant.services;
 
+import com.blibli.future.pos.restaurant.dao.CategoryDAO;
+import com.blibli.future.pos.restaurant.dao.CategoryDAOMysql;
+import com.blibli.future.pos.restaurant.model.Category;
+import com.google.gson.Gson;
 
-public interface ProductService {
-    
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+
+@Path("/categories")
+public class CategoryResource {
+    CategoryDAOMysql categoryDAO;
+
+    public CategoryResource(){
+        categoryDAO = new CategoryDAOMysql();
+    }
+
+    @POST
+    @Consumes("application/json")
+    public Response createCategory(Category category){
+        categoryDAO.create(category);
+        return Response.status(201).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    public Response getAllCategory(){
+        Gson gson = new Gson();
+        String json = gson.toJson(new HashMap<>());
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Category getCategory(@PathParam("id") int id){
+        return new Category();
+    }
 }
--- restaurant/src/main/java/com/blibli/future/pos/restaurant/services/ItemResource.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/services/ItemResource.java
@@ -1,5 +1,5 @@
 package com.blibli.future.pos.restaurant.services;
 
 
-public interface RestaurantService {
+public interface ItemResource {
 }
