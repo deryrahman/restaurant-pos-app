c89d81b907046ad069fe8b012a204860e3bf1fd3
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-11-21 20:57:25 +0700

    Add metadata object

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/Metadata.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/Metadata.java
@@ -1,7 +1,33 @@
 package com.blibli.future.pos.restaurant;
 
-/**
- * Created by dery on 11/21/17.
- */
 public class Metadata {
+    private String count;
+    private String limit;
+
+    public Metadata() {
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getLimit() {
+        return limit;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Metadata{" +
+                "count='" + count + '\'' +
+                ", limit='" + limit + '\'' +
+                '}';
+    }
 }
