f0e6962592b10502308181d03f6182705975d289
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-11-22 22:46:25 +0700

    Implement open and close method

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAO.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAO.java
@@ -3,11 +3,47 @@ package com.blibli.future.pos.restaurant.dao;
 import com.blibli.future.pos.restaurant.Metadata;
 import com.blibli.future.pos.restaurant.model.Category;
 
+import java.util.List;
+
 public interface CategoryDAO {
+    /**
+     * Create category
+     * @param category : only one category will be created
+     * @return true if category is successed to build, false otherwise
+     */
     public boolean create(Category category);
+
+    /**
+     * Get category by id
+     * @param id : integer parameter, id must be valid
+     * @return Category object
+     */
     public Category getById(int id);
-    public Category[] getAll();
-    public Category[] getWithMetadata(Metadata metadata);
-    public boolean delete(Category category);
-    public boolean update(Category category);
+
+    /**
+     * Get all category on database
+     * @return list of category
+     */
+    public List<Category> getAll();
+
+    /**
+     * Get all category with specific limitation
+     * @param metadata : have attributes limit and count.
+     * @return list of filtered category
+     */
+    public List<Category> getWithMetadata(Metadata metadata);
+
+    /**
+     * Delete category
+     * @param id integer : only one category will be deleted. Category must be valid
+     * @return true if success to deleted, false otherwise
+     */
+    public boolean delete(int id);
+
+    /**
+     * Update category
+     * @param id, category : only one category will be updated. Category must be valid
+     * @return true if success to update, false otherwise
+     */
+    public boolean update(int id, Category category);
 }
--- restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAOMysql.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/CategoryDAOMysql.java
@@ -2,57 +2,137 @@ package com.blibli.future.pos.restaurant.dao;
 
 import com.blibli.future.pos.restaurant.Metadata;
 import com.blibli.future.pos.restaurant.MySQLUtility;
+import com.blibli.future.pos.restaurant.dao.util.ErrorHandler;
 import com.blibli.future.pos.restaurant.model.Category;
 import org.apache.commons.dbcp.BasicDataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryDAOMysql implements CategoryDAO {
+    private Connection conn = null;
+    private PreparedStatement ps = null;
+    private ErrorHandler errorHandler = new ErrorHandler();
+
+    private boolean open() {
+        if (conn != null) return true;
+        try {
+            conn = MySQLUtility.getDataSource().getConnection();
+            return true;
+        } catch (SQLException e) {
+//            errorHandler.setMsg("Something wrong on opening connection");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean close() {
+        if (conn == null) return true;
+        try {
+            conn.close();
+            return true;
+        } catch (SQLException e) {
+//            errorHandler.setMsg("Something wrong on closing connection");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     @Override
     public boolean create(Category category) {
-        Connection conn = null;
+        if (!open()) {
+            return false;
+        }
         try {
-            BasicDataSource ds = MySQLUtility.getDataSource();
-            conn = ds.getConnection();
-            System.out.println(category);
-            return true;
-        } catch (Exception e) {
+            String query = "INSERT INTO categories(name, description)" +
+                    " VALUES(?, ?)";
+            ps = conn.prepareStatement(query);
+            ps.setString(1, category.getName());
+            ps.setString(2, category.getDescription());
+
+            int affected = ps.executeUpdate();
+            if (affected > 0) {
+                return true;
+            } else {
+//                errorHandler.setMsg("No affected query. No category inserting");
+            }
+        } catch (SQLException e) {
+//            errorHandler.setMsg("Something wrong on create category");
             e.printStackTrace();
         } finally {
-            if(conn != null){
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+            close();
         }
-        return true;
+        return false;
     }
 
     @Override
     public Category getById(int id) {
-        return null;
+        Category category = new Category();
+        if (!open()) {
+            return category;
+        }
+        try {
+            String query = "SELECT * FROM categories WHERE id = ?";
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            category.setId(rs.getInt("ID"));
+            category.setName(rs.getString("name"));
+            category.setDescription(rs.getString("description"));
+            return category;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return category;
     }
 
     @Override
-    public Category[] getAll() {
-        return new Category[0];
+    public List<Category> getAll() {
+        List<Category> categories = new ArrayList<Category>();
+        if (!open()) {
+            return categories;
+        }
+        try {
+            String query = "SELECT * FROM categories";
+            ps = conn.prepareStatement(query);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Category category = new Category();
+                category.setId(rs.getInt("ID"));
+                category.setName(rs.getString("name"));
+                category.setDescription(rs.getString("description"));
+                categories.add(category);
+            }
+            return categories;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return categories;
     }
 
     @Override
-    public Category[] getWithMetadata(Metadata metadata) {
-        return new Category[0];
+    public List<Category> getWithMetadata(Metadata metadata) {
+        return new ArrayList<Category>();
     }
 
     @Override
-    public boolean delete(Category category) {
-        return false;
+    public boolean delete(int id) {
+        return true;
     }
 
     @Override
-    public boolean update(Category category) {
-        return false;
+    public boolean update(int id, Category category) {
+        return true;
     }
 }
