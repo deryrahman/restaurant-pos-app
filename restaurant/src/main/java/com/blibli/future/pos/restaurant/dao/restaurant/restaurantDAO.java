927453cb2a4a1bc23416a7f54e2c2ff22da5ef0e
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-01 11:29:22 +0700

    Rearrange package

---
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/restaurant/restaurantDAO.java
@@ -0,0 +1,41 @@
+package com.blibli.future.pos.restaurant.dao;
+
+
+public interface restaurantDAO {
+
+    /**
+     * Create category
+     * @param category : only one category will be created
+     * @return true if category is successed to build, false otherwise
+     */
+    public boolean create(Category category);
+
+    /**
+     * Get category by id
+     * @param id : integer parameter, id must be valid
+     * @return Category object
+     */
+    public Category getById(int id);
+
+    /**
+     * Get all category with specific limitation
+     * @param filter : is a WHERE statemtment of mysql query
+     *               To get all categories, just set filter to "true"
+     * @return list of filtered category
+     */
+    public List<Category> getBulk(String filter);
+
+    /**
+     * Delete category
+     * @param id integer : only one category will be deleted. Category must be valid
+     * @return true if success to deleted, false otherwise
+     */
+    public boolean delete(int id);
+
+    /**
+     * Update category
+     * @param id, category : only one category will be updated. Category must be valid
+     * @return true if success to update, false otherwise
+     */
+    public boolean update(int id, Category category);
+}
---
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/restaurant/restaurantMysqlDAO.java
@@ -0,0 +1,7 @@
+package com.blibli.future.pos.restaurant.dao.restaurant;
+
+/**
+ * Created by dery on 12/1/17.
+ */
+public class restaurantMysqlDAO {
+}
