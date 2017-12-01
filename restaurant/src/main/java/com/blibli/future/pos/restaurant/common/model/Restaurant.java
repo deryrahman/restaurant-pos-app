9a6b4b0bcf0373052c33653f7a3857c4a848d39c
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-01 19:11:34 +0700

    Add setter and getter href on Restaurant model

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/common/model/Restaurant.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/common/model/Restaurant.java
@@ -45,6 +45,14 @@ public class Restaurant {
         this.phone = phone;
     }
 
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
     /**
      * Must trigger after set all variable
      */
