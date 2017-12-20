f19e9066527d18504644ad72c0d3d851df9579c3
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-20 09:58:40 +0700

    Modify itemDAO

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/item/ItemDAO.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/item/ItemDAO.java
@@ -8,62 +8,62 @@ import java.util.List;
 public interface ItemDAO {
 
     /**
-     * Create com.blibli.future.pos.restaurant.item
-     * @param item : only one com.blibli.future.pos.restaurant.item will be created
+     * Create item
+     * @param item : only one item will be created
      */
     public void create(Item item) throws SQLException;
 
     /**
-     * Get com.blibli.future.pos.restaurant.item by id
+     * Get item by id
      * @param id : integer parameter, id must be valid
      * @return Item object
      */
-    public Item getById(int id) throws SQLException;
+    public Item findById(int id) throws SQLException;
 
     /**
-     * Get all com.blibli.future.pos.restaurant.item with specific limitation
+     * Get all item with specific limitation
      * @param filter : is a WHERE statemtment of mysql query
      *               To get all items, just set filter to "true"
      * @return list of filtered items
      */
-    public List<Item> getBulk(String filter) throws SQLException;
+    public List<Item> find(String filter) throws SQLException;
 
     /**
-     * Delete com.blibli.future.pos.restaurant.item
-     * @param id integer : only one com.blibli.future.pos.restaurant.item will be deleted. Item must be valid
+     * Delete item
+     * @param id integer : only one item will be deleted. Item must be valid
      */
     public void delete(int id) throws SQLException;
 
     /**
-     * Update com.blibli.future.pos.restaurant.item
-     * @param id, com.blibli.future.pos.restaurant.item: only one com.blibli.future.pos.restaurant.item will be updated. Item must be valid
+     * Update item
+     * @param id, item: only one item will be updated. Item must be valid
      */
     public void update(int id, Item item) throws SQLException;
-
-    /**
-     * Add relation restaurant com.blibli.future.pos.restaurant.item
-     * @param itemId id com.blibli.future.pos.restaurant.item. Id must be valid
-     */
-    public void addRelationRestaurantItem(int itemId, int restaurantId, int stock) throws SQLException;
-
-    /**
-     * Get all com.blibli.future.pos.restaurant.item on specified retaurant
-     * @param restaurantId restaurant id
-     * @return list of com.blibli.future.pos.restaurant.item
-     */
-    public List<Item> getAllItemByRestaurantId(int restaurantId, String filter) throws SQLException;
-
-    /**
-     * Delete relation restaurant com.blibli.future.pos.restaurant.item
-     * @param itemId id com.blibli.future.pos.restaurant.item. Id must be valid
-     */
-    public void deleteRelationRestaurantItem(int itemId, int restaurantId) throws SQLException;
-
-    /**
-     * Update relation
-     * @param itemId id com.blibli.future.pos.restaurant.item
-     * @param restaurantId it restaurant
-     * @param stock count of stock available
-     */
-    public void updateRelationRestaurantItem(int itemId, int restaurantId, int stock) throws SQLException;
+//
+//    /**
+//     * Add relation restaurant item
+//     * @param itemId id item. Id must be valid
+//     */
+//    public void addToRestaurant(int itemId, int restaurantId, int stock) throws SQLException;
+//
+//    /**
+//     * Get all item on specified retaurant
+//     * @param restaurantId restaurant id
+//     * @return list of item
+//     */
+//    public List<Item> getAllItemByRestaurantId(int restaurantId) throws SQLException;
+//
+//    /**
+//     * Delete relation restaurant item
+//     * @param itemId id item. Id must be valid
+//     */
+//    public void deleteRestaurant(int itemId, int restaurantId) throws SQLException;
+//
+//    /**
+//     * Update relation
+//     * @param itemId id item
+//     * @param restaurantId it restaurant
+//     * @param stock count of stock available
+//     */
+//    public void updateRelationRestaurantItem(int itemId, int restaurantId, int stock) throws SQLException;
 }
--- restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/item/ItemDAOMysql.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/dao/item/ItemDAOMysql.java
@@ -1,8 +1,9 @@
 package com.blibli.future.pos.restaurant.dao.item;
 
-import com.blibli.future.pos.restaurant.common.MysqlDAO;
-import com.blibli.future.pos.restaurant.common.TransactionUtility;
+import com.blibli.future.pos.restaurant.common.TransactionHelper;
+import com.blibli.future.pos.restaurant.dao.MysqlDAO;
 import com.blibli.future.pos.restaurant.common.model.Item;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,9 +11,11 @@ import java.util.List;
 
 @SuppressWarnings("ALL")
 public class ItemDAOMysql extends MysqlDAO<Item> implements ItemDAO{
-    
+    private Item item;
+    private List<Item> items;
+
     @Override
-    protected void mappingObject(Item item, ResultSet rs) throws SQLException {
+    public void mappingObject(Item item, ResultSet rs) throws SQLException {
         item.setId(rs.getInt("id"));
         item.setName(rs.getString("name"));
         item.setTimestampCreated(rs.getTimestamp("timestamp_created"));
@@ -20,14 +23,13 @@ public class ItemDAOMysql extends MysqlDAO<Item> implements ItemDAO{
         item.setDescription(rs.getString("description"));
         item.setCategoryId(rs.getInt("category_id"));
         item.setStatus(rs.getString("status"));
-//        item.autoSetHref();
     }
 
     @Override
     public void create(Item item) throws SQLException {
         String query = "INSERT INTO items(name, price, description, category_id, status)" +
                 " VALUES(?, ?, ?, ?, ?)";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
+        ps = TransactionHelper.getConnection().prepareStatement(query);
         ps.setString(1, item.getName());
         ps.setBigDecimal(2, item.getPrice());
         ps.setString(3, item.getDescription());
@@ -41,28 +43,26 @@ public class ItemDAOMysql extends MysqlDAO<Item> implements ItemDAO{
                 item.setId(rs.getInt(1));
             }
         } else {
-            throw new SQLException("No affected query. No com.blibli.future.pos.restaurant.item inserting");
+            throw new SQLException("No affected query. No item inserting");
         }
     }
 
     @Override
-    public Item getById(int id) throws SQLException {
-        Item item = new Item();
-        String query = "SELECT * FROM items WHERE id = ?";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
-        ps.setInt(1, id);
+    public Item findById(int id) throws SQLException {
+        item = new Item();
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        mappingObject(item, rs);
+        items = find("id = "+ id);
+        if(items.size()>0){
+            item = items.get(0);
+        }
         return item;
     }
 
     @Override
-    public List<Item> getBulk(String filter) throws SQLException {
-        List<Item> items = new ArrayList<>();
+    public List<Item> find(String filter) throws SQLException {
+        items = new ArrayList<>();
         String query = "SELECT * FROM items WHERE "+filter;
-        ps = TransactionUtility.getConnection().prepareStatement(query);
+        ps = TransactionHelper.getConnection().prepareStatement(query);
 
         ResultSet rs = ps.executeQuery();
         while(rs.next()) {
@@ -76,12 +76,12 @@ public class ItemDAOMysql extends MysqlDAO<Item> implements ItemDAO{
     @Override
     public void delete(int id) throws SQLException {
         String query = "DELETE FROM items WHERE id = ?";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
+        ps = TransactionHelper.getConnection().prepareStatement(query);
         ps.setInt(1, id);
 
         int affected = ps.executeUpdate();
         if (affected <= 0) {
-            throw new SQLException("No affected query. No com.blibli.future.pos.restaurant.item deleted");
+            throw new SQLException("No affected query. No item deleted");
         }
     }
 
@@ -94,7 +94,7 @@ public class ItemDAOMysql extends MysqlDAO<Item> implements ItemDAO{
                 "category_id = ?," +
                 "status = ? +" +
                 "WHERE id = ?";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
+        ps = TransactionHelper.getConnection().prepareStatement(query);
         ps.setString(1, item.getName());
         ps.setBigDecimal(2, item.getPrice());
         ps.setString(3, item.getDescription());
@@ -104,67 +104,8 @@ public class ItemDAOMysql extends MysqlDAO<Item> implements ItemDAO{
 
         int affected = ps.executeUpdate();
         if (affected <= 0) {
-            throw new SQLException("No affected query. No com.blibli.future.pos.restaurant.item update");
+            throw new SQLException("No affected query. No item update");
         }
     }
 
-    @Override
-    public void addRelationRestaurantItem(int itemId, int restaurantId, int stock) throws SQLException {
-        String query = "INSERT INTO restaurant_item(restaurant_id, item_id, stock)" +
-                " VALUES(?, ?, ?)";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
-        ps.setInt(1, restaurantId);
-        ps.setInt(2, itemId);
-        ps.setInt(3, stock);
-
-        int affected = ps.executeUpdate();
-        if (affected >= 0) {
-            throw new SQLException("No affected query. No com.blibli.future.pos.restaurant.item inserting");
-        }
-    }
-
-    @Override
-    public List<Item> getAllItemByRestaurantId(int restaurantId, String filter) throws SQLException {
-        List<Item> items = new ArrayList<>();
-        String query = "SELECT items.*, restaurants.id, restaurant_item.stock as stock " +
-                "FROM restaurants JOIN restaurant_item JOIN items " +
-                "WHERE restaurants.id=restaurant_item.restaurant_id AND restaurants.id=" +restaurantId+" AND "+
-                filter;
-        ps = TransactionUtility.getConnection().prepareStatement(query);
-
-        ResultSet rs = ps.executeQuery();
-        while(rs.next()) {
-            Item restaurant = new Item();
-            mappingObject(restaurant, rs);
-            items.add(restaurant);
-        }
-        return items;
-    }
-
-    @Override
-    public void deleteRelationRestaurantItem(int itemId, int restaurantId) throws SQLException {
-        String query = "DELETE FROM restaurant_item WHERE restaurant_id = ? AND item_id = ?";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
-        ps.setInt(1, restaurantId);
-        ps.setInt(2, itemId);
-
-        int affected = ps.executeUpdate();
-        if (affected <= 0) {
-            throw new SQLException("No affected query. No com.blibli.future.pos.restaurant.item deleted");
-        }
-    }
-
-    @Override
-    public void updateRelationRestaurantItem(int itemId, int restaurantId, int stock) throws SQLException {
-        String query = "UPDATE restaurant_item SET stock = ? WHERE restaurant_id = ? AND item_id = ?";
-        ps = TransactionUtility.getConnection().prepareStatement(query);
-        ps.setInt(1, stock);
-        ps.setInt(2, restaurantId);
-        ps.setInt(3, itemId);
-
-        int affected = ps.executeUpdate();
-        if (affected <= 0) {
-            throw new SQLException("No affected query. No com.blibli.future.pos.restaurant.item stock update");
-        }
-    }
 }
