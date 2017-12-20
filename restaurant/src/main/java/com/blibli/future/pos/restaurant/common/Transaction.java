fa3107a6d30ee010f20cf59ab306b7cb99c3203c
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-20 10:46:44 +0700

    change SQLException to Exception

--- restaurant/src/main/java/com/blibli/future/pos/restaurant/common/Transaction.java
+++ restaurant/src/main/java/com/blibli/future/pos/restaurant/common/Transaction.java
@@ -1,8 +1,10 @@
 package com.blibli.future.pos.restaurant.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface Transaction {
-    public Object execute(Connection conn) throws SQLException;
+    public Object execute(Connection conn) throws Exception;
 }
