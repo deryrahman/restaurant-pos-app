49a88382d54926827cf0cfd5bd7d536d90db7c6e
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-22 13:59:30 +0700

    Show items on homepage

--- web/src/main/webapp/assets/js/main.js
+++ web/src/main/webapp/assets/js/main.js
@@ -1,22 +1,111 @@
 var coreService = "http://localhost:8080/restaurant"
-
-$(document).ready(function(){
-    // Category
+// Main
+function loadMain(){
+    loadCategories()
+    loadAllItem()
+}
+// Categories
+function loadCategories(){
     $.getJSON(coreService+"/categories", function (data) {
+        if(!data["success"]){
+            console.log(data["message"])
+            return;
+        }
         var payload = data["payload"]
         var categories = []
+        var tabContentCategories = []
         $.each(payload, function (key, val) {
-            var id = "category-"+key
+            var id = val["id"]
             var name = val["name"]
             categories.push(
-                "<li id='"+id+"'>"+
-                    "<a href='#"+id+"' data-toggle='tab'>"+
-                    name+
-                    "</a>"+
-                "</li>"
+                categoryToHTML(id, name)
+            )
+            tabContentCategories.push(
+                tabContentCategoryToHTML(id)
             )
         })
 
         $("#nav-categories").append(categories)
+        $(".tab-content").append(tabContentCategories)
     })
-});
+}
+function categoryToHTML(id, name) {
+    var catId = "category-"+id
+    var result =
+    "<li>"+
+    "<a href='#"+catId+"' data-toggle='tab' onclick='loadItemByCategoryId("+id+")'>"+
+    name+
+    "</a>"+
+    "</li>"
+    return result
+}
+function tabContentCategoryToHTML(id) {
+    var catId = "category-"+id
+    var result =
+    "<div class='tab-pane fade"+"' id='"+catId+"'>"+
+    "</div>"
+    return result
+}
+// Items
+function loadItemByCategoryId(categoryId) {
+    $.getJSON(coreService+"/categories/"+categoryId+"/items", function (data) {
+        if(!data["success"]){
+            console.log(data["message"])
+            return;
+        }
+        var payload = data["payload"]
+        var items = []
+        $.each(payload, function (key, val) {
+            var id = "item-"+key
+            var name = val["name"]
+            var price = val["price"]
+            var categoryId = val["categoryId"]
+            items.push(
+                itemToHTML(id,name,price,categoryId)
+            )
+        })
+
+        $("#category-"+categoryId).empty().append(items)
+    })
+}
+function loadAllItem() {
+    $.getJSON(coreService+"/items", function (data) {
+        if(!data["success"]){
+            console.log(data["message"])
+            return;
+        }
+        var payload = data["payload"]
+        var items = []
+        $.each(payload, function (key, val) {
+            var id = val["id"]
+            var name = val["name"]
+            var price = val["price"]
+            var categoryId = val["categoryId"]
+            items.push(
+                itemToHTML(id,name,price,categoryId)
+            )
+        })
+
+        console.log(items)
+
+        $("#category-all").empty().append(items)
+    })
+}
+function itemToHTML(id, name, price, categoryId) {
+    var itemId = "item"+id
+    var result = "<div class='col-xs-12 col-sm-3 col-md-2 col-lg-3'>"+
+    "<div class='item pointer'>" +
+    "<div class='row padding-default'>" +
+    "<div class='col-xs-8 col-sm-12'>" +
+    "<h4 class='title-item'>" +
+    name +
+    "</h4>" +
+    "</div>" +
+    "<div class='col-xs-4 col-sm-12 align-right price-item'>" +
+    "Rp" + price +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</div>"
+    return result
+}
