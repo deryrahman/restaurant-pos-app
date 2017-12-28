14429578c82baafc6c5e1290495afd07481807b8
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2017-12-28 14:31:33 +0700

    Added cookie checking to adminpage.

--- web/src/main/webapp/assets/adminpage/script.js
+++ web/src/main/webapp/assets/adminpage/script.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+
+    var token = Cookies.get("POSRESTAURANT");
+    var config;
+    var baseUrl;
+    var parserUrl;
+    var userServiceUrl;
+    var itemServiceUrl;
+    var categoryServiceUrl;
+    var restaurantServiceUrl;
+
+    (function () {
+        if (token === undefined) {
+            backToLoginPage("You are not logged in. Please login.");
+        }
+
+        var configUrl = "configurations.json";
+        $.getJSON(configUrl, function (data) {
+            config = data;
+            baseUrl = config.baseUrl;
+            parserUrl = baseUrl + config.endpoints.parser;
+            userServiceUrl = baseUrl + config.endpoints.user;
+            itemServiceUrl = baseUrl + config.endpoints.item;
+            categoryServiceUrl = baseUrl + config.endpoints.category;
+            restaurantServiceUrl = baseUrl + config.endpoints.restaurant;
+        }).done(function () {
+            $.ajax(parserUrl, {
+                method: "POST",
+                contentType: "application/x-www-form-urlencoded",
+                data: {token: token}
+            }).fail(function () {
+                backToLoginPage("Your session is already expired. Please login again.");
+            });
+        });
+    })();
+
+    function backToLoginPage(message) {
+        alert(message);
+        window.location.replace(config.pages.login);
+    }
+
     $('#show-users').click(function () {
         $('#panel-overview').hide();
         $('#panel-items').hide();
