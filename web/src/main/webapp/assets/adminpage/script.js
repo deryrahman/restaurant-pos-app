4d428f5a50e3746a4342126f4660b7a50a5122b2
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2017-12-29 11:02:13 +0700

    Added restaurants and items panel. Heavy refactor on progress.

--- web/src/main/webapp/assets/adminpage/script.js
+++ web/src/main/webapp/assets/adminpage/script.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     var baseUrl;
     var serviceUrls = {};
     var userData = {};
+    var dataLists = {};
+    var tableHeaders = {};
 
     (function () {
         if (token === undefined) {
@@ -48,19 +50,26 @@ $(document).ready(function () {
 
     function initializeAdminPage() {
         setUsername();
-        setCounts();
-        generatePanels();
+        loadAllData();
+        bindShowPanelToClickEvent();
     }
 
     function setUsername() {
         $("#username").html(userData.username);
     }
 
-    function setCounts() {
-        $.get(serviceUrls.user, function (data) {
-            $("#user-count-badge").html(data.payload.length);
-            $("#user-count-well").append(data.payload.length);
-        });
+    function loadAllData() {
+        var userTableHeader =
+            "<tr>\n" +
+            "    <th>#ID</th>\n" +
+            "    <th>Username</th>\n" +
+            "    <th>Role</th>\n" +
+            "    <th>Email</th>\n" +
+            "    <th>Restaurant</th>\n" +
+            "    <th></th>\n" +
+            "</tr>";
+
+        loadData("user", userTableHeader, userToTableRow);
 
         $.get(serviceUrls.restaurant, function (data) {
             $("#restaurant-count-badge").html(data.payload.length);
@@ -72,29 +81,19 @@ $(document).ready(function () {
             $("#category-count-well").append(data.payload.length);
         });
 
-        $.get(serviceUrls.item, function (data) {
-            $("#item-count-badge").html(data.payload.length);
-            $("#item-count-well").append(data.payload.length);
-        });
+        loadItemData();
     }
 
-    function generatePanels() {
-        generateUserPanel();
-        //generateRestaurantPanel();
-        //generateCategoryPanel();
-        //generateItemPanel();
-    }
+    function loadData(dataName, tableHeader, toTableRowFunction) {
+        $.get(serviceUrls[dataName], function (data) {
+            dataLists[dataName] = data.payload;
 
-    function generateUserPanel() {
-        var userList;
+            $("#"+dataName+"-count-badge").html(dataLists[dataName].length);
+            $("#"+dataName+"-count-well").append(dataLists[dataName].length);
 
-        $.get(serviceUrls.user, function (data) {
-            userList = data.payload;
-            console.log(data);
-            console.log(userList);
-
-            userList.forEach(function (item) {
-                $("#user-table").append(userToTableRow(item));
+            $("#"+dataName+"-table").html(tableHeader);
+            dataLists[dataName].forEach(function (item) {
+                $("#"+dataName+"-table").append(toTableRowFunction(item));
             })
         });
     }
@@ -111,40 +110,99 @@ $(document).ready(function () {
         return row;
     }
 
+    function loadRestaurantData() {
+        var restaurantList;
+        var restaurantTableHeader =
+            "<tr>\n" +
+            "    <th>#ID</th>\n" +
+            "    <th>Address</th>\n" +
+            "    <th>Phone</th>\n" +
+            "    <th></th>\n" +
+            "</tr>";
+
+        $.get(serviceUrls.restaurant, function (data) {
+            restaurantList = data.payload;
+
+            $("#restaurant-count-badge").html(restaurantList.length);
+            $("#restaurant-count-well").append(restaurantList.length);
+
+            $("#restaurant-table").html(restaurantTableHeader);
+            restaurantList.forEach(function (user) {
+                $("#restaurant-table").append(userToTableRow(user));
+            })
+        })
+    }
+
+    function userToTableRow(userObject) {
+        row = "<tr>" +
+            "       <td>" + userObject.id + "</td>" +
+            "       <td>" + userObject.name + "</td>" +
+            "       <td>" + userObject.role + "</td>" +
+            "       <td>" + userObject.email + "</td>" +
+            "       <td>" + userObject.restaurantId + "</td>" +
+            "       <td><a role='button'>edit</a></td>" +
+            "</tr>";
+        return row;
+    }
+
+    function loadItemData() {
+        var itemList;
+        var itemTableHeader =
+            "<tr>\n" +
+            "    <th>#ID</th>\n" +
+            "    <th>Item name</th>\n" +
+            "    <th>Price</th>\n" +
+            "    <th>Category</th>\n" +
+            "    <th>Status</th>\n" +
+            "    <th></th>\n" +
+            "</tr>";
+
+        $.get(serviceUrls.item, function (data) {
+            itemList = data.payload;
+
+            $("#item-count-badge").html(itemList.length);
+            $("#item-count-well").append(itemList.length);
+
+            $("#item-table").html(itemTableHeader);
+            itemList.forEach(function (item) {
+                $("#item-table").append(itemToTableRow(item));
+            });
+        });
+    }
+
+    function itemToTableRow(itemObject) {
+        row = "<tr>" +
+            "       <td>" + itemObject.id + "</td>" +
+            "       <td>" + itemObject.name + "</td>" +
+            "       <td>" + itemObject.price + "</td>" +
+            "       <td>" + itemObject.categoryId + "</td>" +
+            "       <td>" + itemObject.status + "</td>" +
+            "       <td><a role='button'>edit</a></td>" +
+            "</tr>";
+        return row;
+    }
+
 
     $('#logout-btn').click(function (e) {
         Cookies.remove("POSRESTAURANT");
         window.location.assign(config.pages.login);
     });
 
-    $('#show-users').click(function () {
-        $('#panel-overview').hide();
-        $('#panel-items').hide();
-        $('#panel-users').show();
+    function bindShowPanelToClickEvent() {
+        showPanelButtonOnclick("overview");
+        showPanelButtonOnclick("users");
+        showPanelButtonOnclick("restaurants");
+        showPanelButtonOnclick("items");
+    }
 
-        $('#show-overview').removeClass('active main-bg-color');
-        $('#show-items').removeClass('active main-bg-color');
-        $('#show-users').addClass('active main-bg-color');
+    function showPanelButtonOnclick(panelName) {
+        $("#show-"+panelName).click(function () {
+            $(".panel.panel-default").hide();
+            $("#panel-"+panelName).show();
 
-    });
+            $("a[id|='show']").removeClass('active main-bg-color');
+            $(this).addClass('active main-bg-color');
+        });
+    }
 
-    $('#show-overview').click(function () {
-        $('#panel-users').hide();
-        $('#panel-items').hide();
-        $('#panel-overview').show();
-
-        $('#show-users').removeClass('active main-bg-color');
-        $('#show-items').removeClass('active main-bg-color');
-        $('#show-overview').addClass('active main-bg-color');
-    });
-
-    $('#show-items').click(function () {
-        $('#panel-users').hide();
-        $('#panel-overview').hide();
-        $('#panel-items').show();
-
-        $('#show-users').removeClass('active main-bg-color');
-        $('#show-overview').removeClass('active main-bg-color');
-        $('#show-items').addClass('active main-bg-color');
-    });
 });
