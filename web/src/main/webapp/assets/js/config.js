ea2d0b5f0f80260554ed1344b6825a7becb01d8e
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-29 20:07:13 +0700

    Add restaurant profile

--- web/src/main/webapp/assets/js/config.js
+++ web/src/main/webapp/assets/js/config.js
@@ -9,6 +9,9 @@ var userId;
 var restaurantId;
 var userRole;
 
+var coreService = "http://localhost:8080/restaurant"
+var authService = "http://localhost:8080/auth"
+
 var configUrl = "configurations.json";
 $.ajax(configUrl, {
     method: "GET",
@@ -26,38 +29,103 @@ $.ajax(configUrl, {
 
 var authService = "http://localhost:8080/auth"
 var coreService = "http://localhost:8080/restaurant"
-var token = Cookies.get('POSRESTAURANT')
+
+function getCookie(cookiename) {
+    var cookiestring=RegExp(""+cookiename+"[^;]+").exec(document.cookie);
+    return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+}
+
+var token = getCookie('POSRESTAURANT')
 
 if(!token){
     window.location.assign(config.pages.login);
+} else {
+    $.ajax(authService+"/parseToken", {
+        method: "POST",
+        url: authService+"/parseToken",
+        contentType: "application/x-www-form-urlencoded",
+        async:false,
+        data: {token: token},
+        success: function (response) {
+            userInfo = response.payload;
+            userId = userInfo["id"]
+            userRole = userInfo["role"]
+            setUserInformation();
+        },
+        error: function(){
+            window.location.assign(config.pages.login);
+        }
+    });
 }
-var userInfo;
-$.ajax(authService+"/parseToken", {
-    method: "POST",
-    url: authService+"/parseToken",
-    contentType: "application/x-www-form-urlencoded",
-    async:false,
-    data: {token: token},
-    success: function (response) {
-        userInfo = response.payload;
-        userId = userInfo["id"]
-        userRole = userInfo["role"]
-    },
-    error: function(){
-        window.location.assign(config.pages.login);
-    }
-});
 
-$.ajax(coreService+"/users/"+userId, {
-    method: "GET",
-    url: coreService+"/users/"+userId,
-    dataType: "json",
-    async:false,
-    success: function (data) {
+function setUserInformation(){
+    getJSON(false,coreService+"/users/"+userId, function (data) {
         var payload = data["payload"]
+        userInfo = payload
         restaurantId = payload["restaurantId"]
-    },
-    error: function(){
-        alert("Restaurant id fail to invoke")
-    }
-});
+    })
+}
+
+function loadErr(str) {
+    return "Load error "+str;
+}
+
+function getJSON(async, url, callback){
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'json',
+        async: async,
+        success: function (data) {
+            callback(data)
+        },
+        error: function (e) {
+            console.log(e.responseJSON.message);
+            alert("Error GET Request!")
+        }
+    });
+}
+function postJSON(async, url, data, callback){
+    $.ajax(url, {
+        data : JSON.stringify(data),
+        contentType : 'application/json',
+        type : 'POST',
+        async : async,
+        success: function (data){
+            callback(data)
+        },
+        error: function (e) {
+            console.log(e.responseJSON.message);
+            alert("Error POST Request!")
+        }
+    });
+}
+function putJSON(async, url, data, callback){
+    $.ajax(url, {
+        data : JSON.stringify(data),
+        contentType : 'application/json',
+        type : 'PUT',
+        async : async,
+        success: function (data){
+            callback(data)
+        },
+        error: function (e) {
+            console.log(e.responseJSON.message);
+            alert("Error PUT Request!")
+        }
+    });
+}
+function deleteJSON(async, url, callback){
+    $.ajax(url, {
+        contentType : 'application/json',
+        type : 'DELETE',
+        async : async,
+        success: function (data){
+            callback(data)
+        },
+        error: function (e) {
+            console.log(e.responseJSON.message);
+            alert("Error PUT Request!")
+        }
+    });
+}
---
+++ web/src/main/webapp/template/managerpage/script.js
@@ -0,0 +1,152 @@
+var users = [];
+var receipts = [];
+var items = [];
+var members = [];
+var categories = [];
+var restaurants = [];
+var restaurant = {};
+
+function getTabList() {
+    var list = [];
+    $('.list-group-item').each(function(){
+        var id = this.id.split("-");
+        var tab = id[id.length-1];
+        list.push(tab);
+    });
+    return list;
+}
+function generateToggle() {
+    var tabList = getTabList();
+    console.log(tabList)
+    $.each(tabList,function (key,item) {
+        $('#show-'+item).click(function(){
+            $.each(tabList,function(key1, item1){
+                if(item!=item1){
+                    $('#panel-'+item1).hide();
+                    $('#show-'+item1).removeClass('active main-bg-color');
+                } else {
+                    $('#panel-'+item1).show();
+                    $('#show-'+item1).addClass('active main-bg-color');
+                }
+            });
+        })
+            .attr('onclick','load'+item+'()');
+    });
+}
+
+function loadScript() {
+    generateToggle();
+    loadoverview();
+}
+
+function loadoverview() {
+
+    loadusers();
+    loaditems();
+    loadreceipts();
+    loadmembers();
+    loadrestaurants();
+}
+
+function addToOverview(){
+    var overviewCount = {
+        'users' : users.length,
+        'items' : items.length,
+        'receipts' : receipts.length,
+        'members' : members.length
+    };
+    $.each(overviewCount, function(key, val){
+        $('#count-'+key).text(val)
+    })
+}
+
+function write() {
+    console.log(users)
+    console.log(items)
+    console.log(receipts)
+    console.log(members)
+    console.log(categories)
+}
+
+function loadusers() {
+    console.log("load users");
+    getJSON(true,coreService+"/users", function(data){
+        var payload = data["payload"];
+        users = payload;
+        addToOverview();
+    });
+}
+
+function loaditems() {
+    console.log("load items");
+    getJSON(true,coreService+"/items", function(data){
+        var payload = data["payload"];
+        items = payload;
+        addToOverview();
+    });
+}
+
+function loadreceipts(){
+    console.log("load receipts");
+    getJSON(true,coreService+"/receipts", function(data){
+        var payload = data["payload"];
+        receipts = payload;
+        addToOverview();
+    });
+}
+
+function loadmembers() {
+    console.log("load members");
+
+    getJSON(true,coreService+"/members", function(data){
+        var payload = data["payload"];
+        members = payload;
+        addToOverview();
+    });
+}
+
+function loadcategories() {
+    console.log("load categories");
+    getJSON(true,coreService+"/categories", function(data){
+        var payload = data["payload"];
+        categories = payload;
+    });
+}
+
+function loadrestaurants() {
+    console.log("load restaurants");
+    getJSON(true,coreService+"/restaurants", function(data){
+        var payload = data["payload"];
+        restaurants = payload;
+        restaurants.forEach(function(data){
+            if(data.id == restaurantId){
+                restaurant = data;
+                console.log(data)
+            }
+        });
+
+        $('.restaurant-id').each(function(){
+            $(this).text(restaurant.id)
+        });
+        $('.restaurant-address').each(function () {
+            $(this).text(restaurant.address)
+        });
+        $('.restaurant-phone').each(function () {
+            $(this).text(restaurant.phone)
+        });
+        $('#address').val(restaurant.address)
+        $('#phone').val(restaurant.phone)
+    });
+}
+
+function saveEditRestaurant(){
+    var restaurant1 = {
+        'address' : $('#address').val(),
+        'phone' : $('#phone').val()
+    };
+    var url = coreService+"/restaurants/"+restaurantId;
+    putJSON(false, url, restaurant1, function() {
+        alert("Success")
+        loadrestaurants()
+    });
+}
