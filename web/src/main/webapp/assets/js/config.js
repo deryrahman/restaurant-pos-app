b351742b968941c1b4028937083449d2b0b54d86
Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
2017-12-29 11:28:19 +0700

    Create draft 1 for manager page

--- web/src/main/webapp/assets/js/config.js
+++ web/src/main/webapp/assets/js/config.js
@@ -4,8 +4,10 @@ var loginUrl;
 var registerUserUrl;
 var registerIdentityUrl;
 
+var userInfo;
 var userId;
 var restaurantId;
+var userRole;
 
 var configUrl = "configurations.json";
 $.ajax(configUrl, {
@@ -19,5 +21,43 @@ $.ajax(configUrl, {
         loginUrl = baseUrl + config.endpoints.login;
         registerIdentityUrl = baseUrl + config.endpoints.register;
         registerUserUrl = baseUrl + config.endpoints.user;
+    }
+});
+
+var authService = "http://localhost:8080/auth"
+var coreService = "http://localhost:8080/restaurant"
+var token = Cookies.get('POSRESTAURANT')
+
+if(!token){
+    window.location.assign(config.pages.login);
+}
+var userInfo;
+$.ajax(authService+"/parseToken", {
+    method: "POST",
+    url: authService+"/parseToken",
+    contentType: "application/x-www-form-urlencoded",
+    async:false,
+    data: {token: token},
+    success: function (response) {
+        userInfo = response.payload;
+        userId = userInfo["id"]
+        userRole = userInfo["role"]
+    },
+    error: function(){
+        window.location.assign(config.pages.login);
+    }
+});
+
+$.ajax(coreService+"/users/"+userId, {
+    method: "GET",
+    url: coreService+"/users/"+userId,
+    dataType: "json",
+    async:false,
+    success: function (data) {
+        var payload = data["payload"]
+        restaurantId = payload["restaurantId"]
+    },
+    error: function(){
+        alert("Restaurant id fail to invoke")
     }
 });
